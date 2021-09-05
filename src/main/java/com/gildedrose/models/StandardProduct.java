226319346965a1954181018b86d5d085cfe49e62
=SAif Awwad <=saif_awwad@apple.com>
2021-09-05 19:20:51 +0300

    Add standard item java class, don't need any custom behavior.

---
+++ src/main/java/com/gildedrose/models/StandardProduct.java
@@ -0,0 +1,41 @@
+package com.gildedrose.models;
+
+import com.gildedrose.Item;
+
+public class StandardProduct implements BasicItem {
+
+    private final Item item;
+
+    public StandardProduct(Item item) {
+        this.item = item;
+    }
+
+    public void updateState() {
+        decreaseSellInValueByOne();
+        if (sellInValueIsOverZero()) {
+            decreaseQualityBy(decreasingValueOverZeroDaysToSell());
+        } else {
+            decreaseQualityBy(decreasingValueForZeroOrLessDaysToSell());
+        }
+    }
+
+    public int decreasingValueOverZeroDaysToSell() {
+        return 1;
+    }
+
+    private void decreaseSellInValueByOne() {
+        item.sellIn -= 1;
+    }
+
+    private boolean sellInValueIsOverZero() {
+        return item.sellIn > 0;
+    }
+
+    private void decreaseQualityBy(int qualityValue) {
+        item.quality -= qualityValue;
+    }
+
+    private int decreasingValueForZeroOrLessDaysToSell() {
+        return decreasingValueOverZeroDaysToSell() * 2;
+    }
+}
