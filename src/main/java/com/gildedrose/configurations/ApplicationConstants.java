f17c272c7688d288cd58227a23bdf5401ead3206
=SAif Awwad <=saif_awwad@apple.com>
2021-09-05 19:42:56 +0300

    Add conjured xonstants

--- src/main/java/com/gildedrose/configurations/ApplicationConstants.java
+++ src/main/java/com/gildedrose/configurations/ApplicationConstants.java
@@ -5,4 +5,5 @@ public class ApplicationConstants {
     public final static String BACKSTAGE_PASSES = "Backstage passes to a" +
         " TAFKAL80ETC concert";
     public final static String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public final static String CONJURED = "Conjured";
 }
